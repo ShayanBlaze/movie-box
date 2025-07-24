85c0f40c6f1ea554afda94e3b31ea49222802259
ShayanBlaze <shayan.13.sharifi@gmail.com>
2025-07-24 21:26:57 +0330

    refactor: update import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import Navbar from "../components/UI/Navbar";
-import SearchOverlay from "../components/UI/SearchOverlay";
-import HomePage from "../components/pages/HomePage";
-import ContentGridPage from "../components/pages/ContentGridPage";
-import DetailPage from "../components/pages/DetailPage";
+import Navbar from './components/UI/Navbar'
+import SearchOverlay from "./components/UI/SearchOverlay";
+import HomePage from "./components/pages/HomePage";
+import ContentGridPage from "./components/pages/ContentGridPage";
+import DetailPage from "./components/pages/DetailPage";
 
 const App = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <div className="bg-[#0c0c0c]">
-      <Navbar onSearchClick={() => setIsSearchOpen(true)} />
+      {/* <Navbar onSearchClick={() => setIsSearchOpen(true)} /> */}
+      <Navbar />
 
       <SearchOverlay
         isOpen={isSearchOpen}
